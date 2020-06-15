fd4ccf4e5bc62f2098eba75833d73c4f21ce0ae4
Paolo Salvatori <leprino@hotmail.com>
2020-06-15 11:55:16 +0200

    Added templates & scripts

---
+++ scripts/check-storage-account.sh
@@ -0,0 +1,16 @@
+subscriptionId=$(az account show --query id --output tsv)
+
+if [[ -z $1 ]]; then
+    echo "The storage account name parameter cannot be null"
+fi
+
+filter="[?name=='$1'].name"
+result=az storage account list --query "$filter"  --output tsv
+
+if [[ -z result ]]
+    echo "The ["$1"] storage account does not exist in the ["$subscriptionId"] subscription"
+    exit 0
+else
+    echo "The ["$1"] storage account already exists in the ["$subscriptionId"] subscription"
+    exit 1
+fi
